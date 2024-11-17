f5c3f7663687d6af7c178023471ae1e4546955ad
Breno <breno.rosas@reidopitaco.com>
2024-11-17 16:12:42 -0300

    feat: add operations to local search

--- algorithms/grasp.hpp
+++ algorithms/grasp.hpp
@@ -3,19 +3,22 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
 #include "../structs/cost.hpp"
 #include "make_random_route.hpp"
 #include "calculate_total_cost.hpp"
 
 using namespace std;
 
-void local_search(vector<vector<Cost>>& adjacency_matrix, vector<int>& route) {
+void local_search(vector<vector<Cost>>& adjacency_matrix, vector<int>& route, Limitations limits) {
     Cost cost = calculate_total_cost(adjacency_matrix, route);
 
     while (true) {
         bool improved = false;
         vector<int> best_route_at_moment = route;
         Cost best_cost_at_moment = cost;
+
+        // 1 - Swap two adjacent nodes
         for (int i = 1; i < best_route_at_moment.size() - 2; i++) {
             swap(best_route_at_moment[i], best_route_at_moment[i + 1]);
             Cost current_cost = calculate_total_cost(adjacency_matrix, best_route_at_moment);
@@ -25,6 +28,40 @@ void local_search(vector<vector<Cost>>& adjacency_matrix, vector<int>& route) {
                 improved = true;
             }
             swap(best_route_at_moment[i], best_route_at_moment[i + 1]);
+        }
+
+        // 2 - Remove a node and insert other that is not in the route
+        unordered_set<int> current_nodes(route.begin(), route.end());
+        for (int i = 1; i < route.size() - 1; i++) {
+            for (int j = 0; j < adjacency_matrix.size(); j++) {
+                if (current_nodes.count(j) == 0) {
+                    vector<int> new_route = route;
+                    new_route.erase(new_route.begin() + i);
+                    new_route.insert(new_route.begin() + i, j);
+                    Cost current_cost = calculate_total_cost(adjacency_matrix, new_route);
+                    if (current_cost.is_inside_limits(limits) && current_cost < best_cost_at_moment) {
+                        best_cost_at_moment = current_cost;
+                        best_route_at_moment = new_route;
+                        improved = true;
+                    }
+                }
+            }
+        }
+
+        // 3 - Add a new node to the route
+        for (int j = 0; j < adjacency_matrix.size(); j++) {
+            if (current_nodes.count(j) == 0) {
+                for (int i = 1; i < route.size(); i++) {
+                    vector<int> new_route = route;
+                    new_route.insert(new_route.begin() + i, j);
+                    Cost current_cost = calculate_total_cost(adjacency_matrix, new_route);
+                    if (current_cost.is_inside_limits(limits) && current_cost < best_cost_at_moment) {
+                        best_cost_at_moment = current_cost;
+                        best_route_at_moment = new_route;
+                        improved = true;
+                    }
+                }
+            }
         }
 
         if (!improved) {
@@ -45,16 +82,11 @@ vector<int> grasp(vector<vector<Cost>>& adjacency_matrix, Limitations limits) {
 
     for (int i = 0; i < max_iterations; i++) {
         vector<int> route = make_random_route(adjacency_matrix, 0.5, limits);
-        local_search(adjacency_matrix, route);
+        local_search(adjacency_matrix, route, limits);
 
         Cost cost = calculate_total_cost(adjacency_matrix, route);
 
-        // Verifica se o custo está dentro dos limites
-        if (cost.dendeInMililiters <= limits.maxDendeInMililiters &&
-            cost.dendeInMililiters >= limits.minDendeInMililiters &&
-            cost.durationSeconds <= limits.maxCollectionTimeInSeconds &&
-            cost < best_cost) {
-
+        if (cost.is_inside_limits(limits) && cost < best_cost) {
             best_cost = cost;
             best_route = route;
         }
--- structs/cost.hpp
+++ structs/cost.hpp
@@ -15,16 +15,44 @@ struct Cost {
         dendeInMililiters += other.dendeInMililiters;
     }
 
+    // Order by distance, then by duration, then by reverse dendeInMililiters
     bool operator<(const Cost& other) const {
-        return distanceMeters < other.distanceMeters;
+        return distanceMeters < other.distanceMeters ||
+               (distanceMeters == other.distanceMeters && durationSeconds < other.durationSeconds) ||
+               (distanceMeters == other.distanceMeters && durationSeconds == other.durationSeconds &&
+                dendeInMililiters > other.dendeInMililiters);
     }
 
-    bool operator>(const Cost& other) const {
-        return distanceMeters > other.distanceMeters;
+    // Order by duration, then by distance, then by reverse dendeInMililiters
+    // bool operator<(const Cost& other) const {
+    //     return durationSeconds < other.durationSeconds ||
+    //            (durationSeconds == other.durationSeconds && distanceMeters < other.distanceMeters) ||
+    //            (durationSeconds == other.durationSeconds && distanceMeters == other.distanceMeters &&
+    //             dendeInMililiters > other.dendeInMililiters);
+    // }
+
+    // Order by reverse dendeInMililiters, then by distance, then by duration
+    // bool operator<(const Cost& other) const {
+    //     return dendeInMililiters < other.dendeInMililiters ||
+    //            (dendeInMililiters == other.dendeInMililiters && distanceMeters < other.distanceMeters) ||
+    //            (dendeInMililiters == other.dendeInMililiters && distanceMeters == other.distanceMeters &&
+    //             durationSeconds < other.durationSeconds);
+    // }
+
+
+    // Collected dendeInMililiters per distanceMeters
+    // bool operator<(const Cost& other) const {
+    //     return dende_per_distance() > other.dende_per_distance();
+    // }
+
+    double dende_per_distance() const {
+        return static_cast<double>(dendeInMililiters) / distanceMeters;
     }
 
-    bool operator==(const Cost& other) const {
-        return distanceMeters == other.distanceMeters && durationSeconds == other.durationSeconds;
+    bool is_inside_limits(Limitations limits) const {
+        return dendeInMililiters <= limits.maxDendeInMililiters &&
+               durationSeconds <= limits.maxCollectionTimeInSeconds &&
+               dendeInMililiters >= limits.minDendeInMililiters;
     }
 };
 
